dedcaa23217c9e1c1ed5ee1f183fd9ce1d96d4b4
Nick Haupka <nick.haupka@outlook.de>
2022-03-26 20:27:15 +0100

    change partition

--- openalex_hpc.sh
+++ openalex_hpc.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH -p fat
+#SBATCH -p medium
 #SBATCH -C scratch
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 16
-#SBATCH --mem-per-cpu=8G
 #SBATCH -t 07:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nick.haupka@sub.uni-goettingen.de
